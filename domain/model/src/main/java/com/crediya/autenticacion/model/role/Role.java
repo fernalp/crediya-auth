e987289e9529c748345b453ed5bbfaa2c36667bd
Fernando Almanza <almanzafernando@hotmail.com>
2025-09-03 22:59:33 -0500

    refactor: update model Role

--- domain/model/src/main/java/com/crediya/autenticacion/model/role/Role.java
+++ domain/model/src/main/java/com/crediya/autenticacion/model/role/Role.java
@@ -1,22 +1,13 @@
 package com.crediya.autenticacion.model.role;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder(toBuilder = true)
 public class Role {
     private Integer id;
     private String name;
     private String description;
-
-    public Role(Integer id, String name, String description) {
-        this.id = id;
-        this.name = name.trim().toUpperCase();
-        this.description = description.trim().toLowerCase();
-    }
-
 }
